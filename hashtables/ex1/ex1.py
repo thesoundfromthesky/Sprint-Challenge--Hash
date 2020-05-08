e7a117c10a0ea04f0c846d662ca0c51926b5f579
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-05-08 11:29:24 -0500

    Ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,33 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    """
+    UPER
+    Find sum of two weights that meets limit.
+    Convert list of weights to dict, the key to be value of list, the value to be index of list.
+    Iterate dict and start from the heaviest weight.
+    Check if key of the heaviest weight - limit exists in dict.
+    Return when condition is met.
+    Return format should be (index of the higher, index of the smaller).
+    """
+    weights_dict = {f"{v}-{i}":i for i,v in enumerate(weights)}
+
+    for cur_weight in weights:
+        print(cur_weight)
+        if limit - cur_weight < 0:
+            continue   
+        limit_left = limit - cur_weight     
+        if limit_left in weights_dict:            
+            limit_left_index = weights_dict[limit_left]
+            if limit_left_index == weights_dict[cur_weight]:
+                continue
+            if weights_dict[cur_weight] > limit_left_index: 
+                return [weights_dict[cur_weight], limit_left_index]
+            else:
+                return [limit_left_index, weights_dict[cur_weight]]
 
     return None
+
+weights = [4, 4]
+
+get_indices_of_item_weights(weights, 2, 8)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,29 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-
+    route=[]
+    
+    #Convert tickets to dict
+    trip ={t.source:t.destination for t in tickets}
+    
+    #find departure point
+    cur = trip["NONE"]
+    #append to route
+    route.append(cur)
+    #loop until cur find "NONE"
+    while cur != "NONE":
+        #find next trip
+        cur = trip[cur]
+        #append to route 
+        route.append(cur)
+    
+    #return route
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+tickets = [ticket_1, ticket_2, ticket_3]
+expected = ["PDX", "DCA", "NONE"]
+result = reconstruct_trip(tickets, 3)
+print(result)
