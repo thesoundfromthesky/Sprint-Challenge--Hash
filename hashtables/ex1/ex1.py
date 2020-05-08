c6ee0367a66c7ea1624706e9cb47c5ec107714b8
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-05-08 12:40:47 -0500

    Ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,32 +4,46 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     """
-    UPER
-    Find sum of two weights that meets limit.
-    Convert list of weights to dict, the key to be value of list, the value to be index of list.
-    Iterate dict and start from the heaviest weight.
-    Check if key of the heaviest weight - limit exists in dict.
-    Return when condition is met.
-    Return format should be (index of the higher, index of the smaller).
+    Return indices of sum of two weights to meet limit
     """
-    weights_dict = {f"{v}-{i}":i for i,v in enumerate(weights)}
 
-    for cur_weight in weights:
-        print(cur_weight)
-        if limit - cur_weight < 0:
-            continue   
-        limit_left = limit - cur_weight     
-        if limit_left in weights_dict:            
-            limit_left_index = weights_dict[limit_left]
-            if limit_left_index == weights_dict[cur_weight]:
-                continue
-            if weights_dict[cur_weight] > limit_left_index: 
-                return [weights_dict[cur_weight], limit_left_index]
+    #convert weights to dict to assign to w_d
+    w_d = {}
+    #i is index of weight v is value of weight
+    for i,v in enumerate(weights):
+        #if value is in w_d
+        if v in w_d:
+            #append index of weight to w_d with key value of weight
+            w_d[v].append(i)
+        else:
+            #initialize w_d at key value of weight with value index of weight
+            w_d[v]=[i]
+    
+    #iterate weights
+    for i in weights:
+        #l is limit - i which is weight left reach limit
+        l = limit - i
+        # continue if l is not positive
+        if l < 0:
+            continue
+        #if l exists in w_d process
+        if l in w_d:
+            a,b = None, None       
+            #if l is itself     
+            if l == i:
+                a = w_d[i].pop()
+                b = w_d[i].pop() 
             else:
-                return [limit_left_index, weights_dict[cur_weight]]
+                a = w_d[l].pop()
+                b = w_d[i].pop()
+            #greater index comes first
+            if a > b:
+                return (a, b)
+            else:
+                return (b, a)    
 
     return None
 
 weights = [4, 4]
 
-get_indices_of_item_weights(weights, 2, 8)
+print(get_indices_of_item_weights(weights, 2, 8))
