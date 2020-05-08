953b6041d7a960b7903a1d9d2e6389b47ffcbcf8
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-05-08 12:59:57 -0500

    little change on ex1

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -7,11 +7,11 @@ def intersection(arrays):
     d = {}
     l = len(arrays)
     for i in arrays:
-        for i,v in enumerate(i):
-            if v in d:
-                d[v].append(v)
+        for i in i:
+            if i in d:
+                d[i].append(i)
             else:
-                d[v] = [v]
+                d[i] = [i]
 
     result=[]
 
@@ -22,16 +22,28 @@ def intersection(arrays):
 
     return result
 
-intersection([
-            [1,2,3],
-            [1,4,5],
-            [1,6,7]
-        ])
-# if __name__ == "__main__":
-#     arrays = []
+# intersection([
+#             [1,2,3],
+#             [1,4,5],
+#             [1,6,7]
+#         ])
+        # arrays = [
+        #     list(range(1000000, 2000000)) + [1,2,3],
+        #     list(range(2000000, 3000000)) + [1,2,3],
+        #     list(range(3000000, 4000000)) + [1,2,3],
+        #     list(range(4000000, 5000000)) + [1,2,3],
+        #     list(range(5000000, 6000000)) + [1,2,3],
+        #     list(range(6000000, 7000000)) + [1,2,3],
+        #     list(range(7000000, 8000000)) + [1,2,3],
+        #     list(range(8000000, 9000000)) + [1,2,3],
+        #     list(range(9000000, 10000000)) + [1,2,3],
+        #     list(range(10000000, 11000000)) + [1,2,3]
+        # ]
+if __name__ == "__main__":
+    arrays = []
 
-#     arrays.append(list(range(1000000,2000000)) + [1,2,3])
-#     arrays.append(list(range(2000000,3000000)) + [1,2,3])
-#     arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000,2000000)) + [1,2,3])
+    arrays.append(list(range(2000000,3000000)) + [1,2,3])
+    arrays.append(list(range(3000000,4000000)) + [1,2,3])
 
-#     print(intersection(arrays))
+    print(intersection(arrays))
